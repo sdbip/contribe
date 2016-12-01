1ed016a79745be8dbc3b13745b43eced42372534
Johan Segerfeldt <johan.segerfeldt@me.com>
2016-12-01 09:39:49 +0100

    Add JUnit 5 (with libs) and a failing test to show that it runs.

---
+++ tests/com/contribe/arbetsprov/BookStoreTests.java
@@ -0,0 +1,12 @@
+package com.contribe.arbetsprov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BookStoreTests {
+    @Test
+    public void failingTest() {
+        fail("This is the first test");
+    }
+}
