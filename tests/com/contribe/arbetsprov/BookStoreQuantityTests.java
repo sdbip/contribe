8870a083bc16bcbd7b774476d80f4a23fdd490d0
Johan Segerfeldt <johan.segerfeldt@me.com>
2016-12-01 11:01:59 +0100

    Rename test suite

--- tests/com/contribe/arbetsprov/BookStoreQuantityTests.java
+++ tests/com/contribe/arbetsprov/BookStoreQuantityTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.expectThrows;
 
-public class BookStoreTests {
+public class BookStoreQuantityTests {
 
     private BookStore store;
     private TestInventory inventory;
